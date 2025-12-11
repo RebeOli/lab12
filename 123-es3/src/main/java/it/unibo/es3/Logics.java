cd11eac4c98ce232e3a63e7cc9cf1a2dc5da751c
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2025-12-11 13:08:36 +0100

    Ex 123: Added Logics and LogicsImpl

---
+++ 123-es3/src/main/java/it/unibo/es3/Logics.java
@@ -0,0 +1,30 @@
+package it.unibo.es3;
+
+/**
+ * Interface defining the logic.
+ */
+public interface Logics {
+
+    /**
+     * The number of slots.
+     *
+     * @return the number of slots
+     */
+    int size();
+
+    /**
+     * Change the value of a button.
+     *
+     * @param pair coordinates of the button.
+     * @return the new value a button should show after being pressed.
+     */
+    String hit(Pair<Integer, Integer> pair);
+
+    /**
+     * True if it is time to quit (i.e., one of the row or column is full).
+     * 
+     * @param pair coordinates of the button.
+     * @return whether it is time to quit.
+     */
+    boolean toQuit(Pair<Integer, Integer> pair);
+}
